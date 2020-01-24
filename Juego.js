e1249965736875faaead005c829511f2d3bf699a
Santiago Barrios <santiagobarriosgrijalba.08@gmail.com>
2020-01-24 16:38:33 -0500

    secuencia generada aleatoria

--- Juego.js
+++ Juego.js
@@ -1,19 +1,31 @@
-const celeste = document.getElementById('celeste')
-const violeta = document.getElementById('violeta')
-const naranja = document.getElementById('naranja')
-const verde = document.getElementById('verde')
-const btnEmpezar = document.getElementById('btnEmpezar')
+const violeta = document.getElementById('violeta');
+const naranja = document.getElementById('naranja');
+const celeste = document.getElementById('celeste');
+const verde = document.getElementById('verde');
+const btnEmpezar = document.getElementById('btnEmpezar');
 
 class Juego {
   constructor() {
-    this.inicializar()
+    this.inicializar();
+    this.generarSecuencia();
   }
 
   inicializar() {
-    btnEmpezar.classList.add('hide')
+    btnEmpezar.classList.add('hide');
+    this.nivel = 1;
+    this.colores = {
+      celeste, 
+      violeta,
+      naranja,
+      verde
+    }
+  }
+
+  generarSecuencia(){
+    this.secuencia = new Array (10).fill(0).map(n => Math.floor(Math.random() * 5));
   }
 }
 
 function empezarJuego() {
-  var juego = new Juego()
+  window.juego = new Juego();
 }
